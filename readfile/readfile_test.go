407b0b6949a441725f3d2021aa6e9606b0891c6a
louis <kwezi.louishg@gmail.com>
2023-09-01 03:52:47 +0300

    testing

--- readfile/readfile_test.go
+++ readfile/readfile_test.go
@@ -7,53 +7,53 @@ import (
 )
 
 func TestReadTxt(t *testing.T) {
+	t.Parallel()
 	tests := []struct {
-		name   string
-		input  string
-		want    []string
+		name  string
+		input string
+		want  []string
 	}{
 		{
-		"multiplelines",
-		"codebits\ncodeclinic\njumia\n",
-		[]string{"codebits","codeclinic", "jumia"},
-	},
-  {
-		"singleline",
-		"foodhub",
-		[]string{"foodhub"},
-	},
-
-}
-for _,tc := range tests {
-
-	t.Run(tc.name,func(t *testing.T){
-
-		tempFile,err := os.CreateTemp("", "testfile.txt")
-
-		if err != nil {
-			t.Fatalf("Failed to create temporary file %v",err)
+			"multiplelines",
+			"codebits\ncodeclinic\njumia\n",
+			[]string{"codebits", "codeclinic", "jumia"},
+		},
+		{
+			"singleline",
+			"foodhub",
+			[]string{"foodhub"},
+		},
 	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			tempFile, err := os.CreateTemp("", "testfile.txt")
 
-	defer os.Remove(tempFile.Name())
+			if err != nil {
+				t.Fatalf("Failed to create temporary file %v", err)
+			}
 
-	defer tempFile.Close()
+			defer os.Remove(tempFile.Name())
 
-	_,err = tempFile.WriteString(tc.input)
-	if err != nil {
-		t.Fatalf("Failed to write test data to temporary test file: %v", err)
+			defer tempFile.Close()
+
+			_, err = tempFile.WriteString(tc.input)
+			if err != nil {
+				t.Fatalf("Failed to write test data to temporary test file: %v", err)
+			}
+
+			result := readfile.ReadTxt(tempFile.Name())
+			if len(result) != len(tc.want) {
+				t.Errorf("Expected %d lines,but got %d lines", len(tc.want), len(result))
+			}
+
+			for i := range tc.want {
+				if result[i] != tc.want[i] {
+					t.Errorf("%s: Expected line %d to be \"%s\", but got \"%s\"", tc.name, i+1, tc.want[i], result[i])
+				}
+			}
+		})
+
 	}
-
-	result := readfile.ReadTxt(tempFile.Name())
-	if len(result) != len(tc.want) {
-		t.Errorf("Expected %d lines,but got %d lines", len(tc.want),len(result))
-	}
-	
-	for i := range tc.want {
-		if result[i] != tc.want[i] {
-			t.Errorf("%s: Expected line %d to be \"%s\", but got \"%s\"",tc.name,i+1,tc.want[i],result[i])
-		}
-	}
-	})
-		 
-}
 }
